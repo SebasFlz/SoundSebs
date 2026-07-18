db5c37b467ec9fb251a5234df68df4c6ce800abc
SebasFlz <162603582+SebasFlz@users.noreply.github.com>
2026-07-18 11:00:06 -0500

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,12 +1,13 @@
 export default function handler(req, res) {
-  const scope = 'user-modify-playback-state';
+  const scope = 'user-modify-playback-state user-read-playback-state user-read-currently-playing';
   const redirectUri = 'https://' + req.headers.host + '/api/callback';
 
   const params = new URLSearchParams({
     response_type: 'code',
     client_id: process.env.SPOTIFY_CLIENT_ID,
     scope: scope,
-    redirect_uri: redirectUri
+    redirect_uri: redirectUri,
+    show_dialog: 'true'
   });
 
   res.redirect('https://accounts.spotify.com/authorize?' + params.toString());
